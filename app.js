2bf921e560aa4b375e875ea1f43645aa475db878
Abdumajodov Laziz <153080975+Laziz0627@users.noreply.github.com>
2024-04-20 16:31:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let ism = 'Lazizbek';
+console. log(ism);
+let famila = 'abdumajidov';
+console. log(famila);
+let men = 'telefon';
+console. log(men);
+let ozim = 'men';
+console. log(ozim)
+let firstName = 'lazizbel'
+console. log(firstName)
+let age = 23;
+let price =19.999;
+console. log(age)
+console. log(price)
